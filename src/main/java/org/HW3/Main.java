ecb5daa845f0e6f80b6051ed820635f1beabc4d9
stvjbs <yarashukartem@icloud.com>
2024-02-19 14:06:10 +0100

    ready HW3

--- src/main/java/org/HW3/Main.java
+++ src/main/java/org/HW3/Main.java
@@ -7,12 +7,14 @@ public class Main {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         Student student = new Student("Steve Jobs", 23, 98);
         String fileName = "ser";
+
         System.out.println(student);
-        Program.serialObj(student,fileName);
+
+        Program.serialObj(student, fileName);
         student = (Student) Program.deserialObj(fileName);
+
         System.out.println(student);
     }
-
 
 
 }
--- src/main/java/org/HW3/Program.java
+++ src/main/java/org/HW3/Program.java
@@ -5,7 +5,7 @@ import java.io.*;
 public class Program {
     public static void serialObj(Object object, String filename) {
         try {
-            FileOutputStream fileOutputStream = new FileOutputStream("ser");
+            FileOutputStream fileOutputStream = new FileOutputStream(filename);
             ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
             objectOutputStream.writeObject(object);
             objectOutputStream.close();
@@ -14,7 +14,7 @@ public class Program {
         }
     }
 
-    public static Object deserialObj(String filename) throws IOException, ClassNotFoundException, IOException {
+    public static Object deserialObj(String filename) throws ClassNotFoundException, IOException {
         FileInputStream fileInputStream = new FileInputStream(filename);
         ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
         return objectInputStream.readObject();
